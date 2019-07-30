c02fe6c97361ae431965108c4f94808d356285ce
Colby Taperts <colbytaperts@gmail.com>
2019-07-29 21:45:17 -0500

    on remove ensure settings/posts are reset

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -68,6 +68,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   onDelete(postId: string) {
     this.isLoading = true;
     this.postsService.deletePost(postId).subscribe(() => {
+      this.postsAndSettings = {};
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
     }, () => {
       this.isLoading = false;
